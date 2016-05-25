211ec7010b609f3ea93886f5dbcc8da17920a283
ttt2424 <ttt.smt2424@gmail.com>
2016-05-25 15:13:34 +0900

    最初の！

---
+++ Assets/battleScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class battleScript : MonoBehaviour {
+
+    public void hardLoad()
+    {
+        SceneManager.LoadScene("dummyBattle");
+    }
+
+    public void normalLoad()
+    {
+        SceneManager.LoadScene("dummyBattle");
+    }
+
+    public void easylLoad()
+    {
+        SceneManager.LoadScene("dummyBattle");
+    }
+
+    public void SeceneModeLoad()
+    {
+        SceneManager.LoadScene("mode");
+    }
+}
---
+++ Assets/titleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class titleScript : MonoBehaviour {
+	
+	public void SceneModeLoad () {
+        SceneManager.LoadScene("mode");
+	}
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
