b908839f2d9ec6404b2ed994095ac112a3760a1b
metenryu <t13304050@gunma-u.ac.jp>
2016-05-25 16:45:30 +0900

    スペースで視点切り替え

---
+++ Assets/Camera/changeCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeCamera : MonoBehaviour {
+	Camera mainCamera;
+	Camera subCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+		subCamera  = GameObject.Find("Sub Camera").GetComponent<Camera>();
+
+		subCamera.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if(mainCamera.enabled) {
+				mainCamera.enabled = false;
+				subCamera.enabled = true;
+			} else {
+				mainCamera.enabled = true;
+				subCamera.enabled = false;
+			}
+		}
+	}
+}
