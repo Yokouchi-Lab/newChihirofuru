4df8c5ce72be95902367f518c00d8dac650a8134
odenya <t13304079@gunma-u.ac.jp>
2016-06-09 22:15:00 +0900

    FudaBattleのシーンで

--- Assets/FudaScript/MoveFuda.cs
+++ Assets/FudaScript/MoveFuda.cs
@@ -6,18 +6,22 @@ public class MoveFuda : MonoBehaviour {
     private Vector3 offset;
 
     void OnMouseDown() {
-      if(this.tag == "playerfuda"){
-        this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+      if(GameObject.FindWithTag("check") == null){
+        if(this.tag == "playerfuda"){
+          this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+          this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
       }
     }
 
     void OnMouseDrag() {
-      if(this.tag == "playerfuda"){
-        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
-        transform.position=(new Vector3(Mathf.Clamp(currentPosition.x,0,112),Mathf.Clamp(currentPosition.y,0,0),Mathf.Clamp(currentPosition.z,-40,-20)));
-        //transform.position = currentPosition;
+      if(GameObject.FindWithTag("check") == null){
+        if(this.tag == "playerfuda"){
+          Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+          Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+          transform.position=(new Vector3(Mathf.Clamp(currentPosition.x,0,112),Mathf.Clamp(currentPosition.y,0,0),Mathf.Clamp(currentPosition.z,-40,-20)));
+          //transform.position = currentPosition;
+        }
       }
     }
 
---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour{
+    AudioSource audioSource;
+    public AudioClip audioClip;
+    public AudioClip audioClip1;
+    public AudioClip audioClip2;
+    public AudioClip audioClip3;
+
+    void Start(){
+        int rdm = Random.Range(0, 2);
+        if (rdm == 0)
+            audioClip = audioClip1;
+        else if (rdm == 1)
+            audioClip = audioClip2;
+        else
+            audioClip = audioClip3;
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.A) == true){
+            // Torigger
+            Debug.Log("A!");
+            audioSource.Play();
+        }
+    }
+}
