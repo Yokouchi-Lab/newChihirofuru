e5cd33fa14c46f3928c28accda715599be677973
odenya <t13304079@gunma-u.ac.jp>
2016-06-13 17:22:36 +0900

    結果にコミット

---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+Text text;
+
+// Use this for initialization
+void Start () {
+
+	//text = this.GetComponent ();
+
+}
+
+// Update is called once per frame
+void Update () {
+//	timer += Time.deltaTime;
+//text.text = timer.ToString ();
+	}
+}
