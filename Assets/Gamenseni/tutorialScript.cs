d0e9e89cb195f0dd9c9d39a495bb6c7f2016967a
ttt2424 <ttt.smt2424@gmail.com>
2016-06-13 16:18:00 +0900

    遊び方からタイトル行けるようにした

--- Assets/Gamenseni/tutorialScript.cs
+++ Assets/Gamenseni/tutorialScript.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class tutorialScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void SceneTitleLoad()
+	{
+			SceneManager.LoadScene("title");
 	}
 }
