5e4d9917cc5fd7fed06294b5e01d6526efe77c45
yushi82 <t13304029@gunma-u.ac.jp>
2016-06-21 20:17:05 +0900

    ok

--- Assets/voice/Voice.cs
+++ Assets/voice/Voice.cs
@@ -1,26 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class voices{
+    public AudioClip voice;
+    public float preTime;
+    public float postTime;
+    public float timeOut;
+}
+
 public class Voice : MonoBehaviour {
     AudioSource audioSource;
-    public AudioClip[] voiceArray = new AudioClip[100]; //VoiceData
+    public voices[] voiceArray = new voices[100];
     public AudioClip joka = new AudioClip();
     int[] rdmArray = new int[100]; //乱数
-    public int num = 0, j = 0;
-    public float timeOut = 25;
+    public int num = 0;
+    private int i, j = 0, up = 0;
+    private float timeOut82 = 25.0f;
     private float timeElapsed;
 
-	void Start(){
-        for (int i = 0; i < rdmArray.Length; i++)
+    void Start(){
+        for (i = 0; i < rdmArray.Length; i++)
             rdmArray[i] = i;
         int tmp = 0, random = 0;
-        for (int i = 0; i < rdmArray.Length; i++){
+        for (i = 0; i < rdmArray.Length; i++){
             random = Random.Range(0, 100);
             tmp = rdmArray[i];
             rdmArray[i] = rdmArray[random];
             rdmArray[random] = tmp;
         }
-        for (int i = 0; i < rdmArray.Length; i++)
+        for (i = 0; i < rdmArray.Length; i++)
             Debug.Log(rdmArray[i]);
         audioSource = gameObject.GetComponent<AudioSource>();
         audioSource.clip = joka;
@@ -29,12 +38,30 @@ public class Voice : MonoBehaviour {
 
     void Update(){
         timeElapsed += Time.deltaTime;
-        if (timeElapsed >= timeOut){
-            num = rdmArray[j];
-            audioSource.clip = voiceArray[num];
-            audioSource.Play();
-            j++;
-            timeElapsed = 0.0f;
+        if(up == 0){
+            if(timeElapsed >= timeOut82){
+                num = rdmArray[j];
+                audioSource.clip = voiceArray[num].voice;
+                audioSource.Play();
+                j++;
+                up = 1;
+                timeElapsed = 0.0f;
+                Debug.Log(voiceArray[num].voice);
+                Debug.Log(voiceArray[num].preTime);
+                Debug.Log(voiceArray[num].postTime);
+            }
+        }
+        else{
+            if (timeElapsed >= timeOut82){
+                num = rdmArray[j];
+                audioSource.clip = voiceArray[num].voice;
+                audioSource.Play();
+                j++;
+                timeElapsed = 0.0f;
+                Debug.Log(voiceArray[num].voice);
+                Debug.Log(voiceArray[num].preTime);
+                Debug.Log(voiceArray[num].postTime);
+            }
         }
     }
 }
