2109fa80a179b2a837cd0b5a9b6e6af5298d67e6
ttt2424 <ttt.smt2424@gmail.com>
2016-06-28 14:51:50 +0900

    べんきょう

---
+++ Assets/Gamenseni/Panel.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+
+  static Panel _panel;
+  void Start () {
+    _panel = GetComponent<Panel>();
+  }
+
+  /// 表示・非表示を設定
+  public static void SetActive(string name, bool b) {
+    foreach(Transform child in _panel.transform) {
+      // 子の要素をたどる
+      if(child.name == name) {
+        // 指定した名前と一致
+        // 表示フラグを設定
+        child.gameObject.SetActive(b);
+        // おしまい
+        return;
+      }
+    }
+    // 指定したオブジェクト名が見つからなかった
+    Debug.LogWarning("Not found objname:"+name);
+  }
+}
---
+++ Assets/Gamenseni/toggleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class toggleScript : MonoBehaviour {
+
+/*  Toggle toggle;
+
+  void Start(){
+    toggle = GetComponent<Toggle>();
+  }
+
+  public void textOnof(){
+    if(toggle.isOn) on();
+    else of();
+  }
+
+  public void on(){
+    Button.SetActive(true);
+	}
+
+  public void of(){
+    Button.SetActive(false);
+  }*/
+}
