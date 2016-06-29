47d1e3b220a4879da7f81e069f552df6e67608fd
metenryu <t13304050@gunma-u.ac.jp>
2016-06-29 15:18:19 +0900

    Enemyyy

--- Assets/Camera/Enemy.cs
+++ Assets/Camera/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 	// 現在の音声の番号(1~100)
-	[SerializeField] private int voiceNum = 0;
+	[SerializeField] private int voiceNum = 100;
 	// オブジェクト
 	private GameObject voice;
 	[SerializeField] private GameObject[] playerfuda = new GameObject[25];
@@ -13,8 +13,7 @@ public class Enemy : MonoBehaviour {
 	// 0: なし
 	// 1: プレイヤー札
 	// 2: エネミー札
-	// 最初(voiceNumが0ののとき)の場合も入れて、他のと違って番号-1じゃなくて番号そのまま指定
-	[SerializeField] private int[] existFuda = new int[101];
+	[SerializeField] private int[] existFuda = new int[100];
 
 	// Use this for initialization
 	void Start () {
@@ -23,9 +22,9 @@ public class Enemy : MonoBehaviour {
 			playerfuda = GameObject.FindGameObjectsWithTag ("playerfuda");
 			enemyfuda = GameObject.FindGameObjectsWithTag ("enemyfuda");
 			// プレイヤー札
-			existFuda [ playerfuda [i].GetComponent<FudaData> ().fudanum ] = 1;
+			existFuda [ playerfuda [i].GetComponent<FudaData> ().fudanum - 1 ] = 1;
 			// エネミー札
-			existFuda [ enemyfuda [i].GetComponent<FudaData> ().fudanum ] = 2;
+			existFuda [ enemyfuda [i].GetComponent<FudaData> ().fudanum - 1 ] = 2;
 		}
 
 	}
@@ -33,25 +32,27 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		voiceNum = voice.GetComponent<Voice> ().num;
-		if (existFuda [voiceNum] != 0) {
-			// とりあえずオブジェクト消すだけ
-			// ここで別のメソッドとかに飛ばすとよいのでは
-			//targetFudaNum = fuda [voiceNum-1].GetComponent<FudaData> ().fudanum;
-			//Destroy (fuda [voiceNum-1]);
-			// ******************************
-			Level0();
-			// ******************************
+		if (voiceNum != 100) {
+			if (existFuda [voiceNum] != 0) {
+				// とりあえずオブジェクト消すだけ
+				// ここで別のメソッドとかに飛ばすとよいのでは
+				//targetFudaNum = fuda [voiceNum].GetComponent<FudaData> ().fudanum;
+				//Destroy (fuda [voiceNum]);
+				// ******************************
+				Level0 ();
+				// ******************************
+			}
 		}
 	}
 
 	void Level0() {
-		Debug.Log ("Delete VoiceNum is " + (voiceNum));
+		Debug.Log ("Delete VoiceNum is " + (voiceNum+1));
 		// 決まり字までの時間+5.0秒後に実行する ここの5.0f変数にしてトップにまとめると変更簡単そう
-		float delayTime = voice.GetComponent<Voice> ().voiceArray[voiceNum-1].preTime + 5.0f;
+		float delayTime = voice.GetComponent<Voice> ().voiceArray[voiceNum].preTime + 5.0f;
 		Debug.Log ("delayTime is " + delayTime);
 
 		// 先にプレイヤーに取られるの前提だし、trycatch？みたいのにしたほうがいいかも
-		Destroy( GameObject.Find ("Fuda" + (voiceNum)), delayTime );
+		Destroy( GameObject.Find ("Fuda" + (voiceNum+1)), delayTime );
 
 		existFuda [voiceNum] = 0;
 	}
