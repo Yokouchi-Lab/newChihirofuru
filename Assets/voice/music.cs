f180a47f885bacef10c51b412e43cc404f561dc6
yushi82 <t13304029@gunma-u.ac.jp>
2016-06-29 17:10:24 +0900

    ok

---
+++ Assets/voice/music.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class music : MonoBehaviour {
+    public bool DontDestroyEnabled = true;
+
+    void Start () {
+        if (DontDestroyEnabled){
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad(this);
+        }
+    }
+	
+	void Update () {
+	
+	}
+}
