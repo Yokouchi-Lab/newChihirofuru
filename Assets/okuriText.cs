c65a668166226dac15a6e79231e6dc1d80762dcb
odenya <t13304079@gunma-u.ac.jp>
2016-07-20 15:15:10 +0900

    細かい修正

---
+++ Assets/okuriText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class okuriText : MonoBehaviour {
+	public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		text = GameObject.Find("ot");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(GameObject.FindWithTag("checkokuricom") != null){
+			text.SetActive(false);
+		}
+		if(GameObject.FindWithTag("checkokuricom") == null){
+			text.SetActive(true);
+		}
+
+	}
+}
